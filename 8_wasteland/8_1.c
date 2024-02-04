35dd54c0fe430a6540393170a36edd9d7665d1d9
vasapaun <vasapaunovic674@gmail.com>
2024-02-04 16:44:51 +0100

    finished raw map reading from file day 8 part 1

--- 8_wasteland/8_1.c
+++ 8_wasteland/8_1.c
@@ -14,19 +14,46 @@
     } while (0)
 
 #define LR_LENGTH   (307)
+#define MAP_LENGTH  (714)
 
 
 int main(int argc, char **argv)
 {
     check_error(argc == 2, "argc");
 
+    char* tmp = (char*)malloc(10 * sizeof(char));
+
+    // allocate space needed for raw map and lr instructions
     char* raw_lr = (char*) malloc (LR_LENGTH * sizeof(char));
-    check_error(raw_lr != NULL, "malloc");
+    char** raw_map_keys =    (char**) malloc(MAP_LENGTH * sizeof(char*));
+    char** raw_map_lvalues = (char**) malloc(MAP_LENGTH * sizeof(char*));
+    char** raw_map_rvalues = (char**) malloc(MAP_LENGTH * sizeof(char*));
+    check_error(raw_map_keys != NULL && raw_map_lvalues != NULL && raw_map_rvalues != NULL && raw_lr != NULL, "malloc");
+    for(int i = 0; i < MAP_LENGTH; i++)
+    {
+        raw_map_keys[i] =    (char*) malloc(3 * sizeof(char));
+        raw_map_lvalues[i] = (char*) malloc(3 * sizeof(char));
+        raw_map_rvalues[i] = (char*) malloc(3 * sizeof(char));
+        check_error(raw_map_keys[i] != NULL && raw_map_lvalues[i] != NULL && raw_map_rvalues[i] != NULL, "malloc");
+    }
 
     FILE* input = fopen(argv[1], "r");
     fscanf(input, "%s", raw_lr);
 
-    // TODO: process whole map before closing input file
+    for(int i = 0; i < MAP_LENGTH; i++)
+    {
+        fscanf(input, "%s", tmp);
+        for(int j = 0; j < 3; j++) raw_map_keys[i][j] = tmp[j];
+
+        fscanf(input, "%s", tmp); // discard "="
+
+        fscanf(input, "%s", tmp);
+        for(int j = 0; j < 3; j++) raw_map_lvalues[i][j] = tmp[j + 1];
+
+        fscanf(input, "%s", tmp);
+        for(int j = 0; j < 3; j++) raw_map_rvalues[i][j] = tmp[j];
+        printf("%s %s %s\n", raw_map_keys[i], raw_map_lvalues[i], raw_map_rvalues[i]);
+    }
     fclose(input);
 
     // process LR instructions [translate them to binary - L = 0, R = 1]
